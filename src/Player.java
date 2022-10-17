a9454803748182630dfc748a17c038570130c470
Sergio Medina <63879459+sergiomedinaio@users.noreply.github.com>
2022-10-17 10:20:55 -0300

    feat: add player class

---
+++ src/Player.java
@@ -0,0 +1,23 @@
+public class Player {
+    private String name;
+    private String position;
+    private int number;
+    private int goalCounter;
+    private int matchCounter;
+
+    //NAME SETTER
+    public void setName(String name){
+        this.name = name;
+        return;
+    }
+
+    //NAME GETTER
+    public String getName() {
+        return this.name;
+    }
+    //NAME GETTER
+    public void setPosition(String position) {
+        this.position = position;
+        return;
+    }
+}  
